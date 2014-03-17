caea762d41aa56d61ae079c47306cee872d04454
Nikolay Frik <nfrik@iphonedreams.com>
2014-03-16 21:56:28 -0400

    Fixed bug in pxysingfolder
    Fixed bug on data variable initializations

--- pxysingfolder.py
+++ pxysingfolder.py
@@ -17,20 +17,23 @@ import pylab as P
    #read filenames of lammps executables from metafile
 rootdirpath=os.getcwd()
 
-with open('../python_data/dir1/metadat.txt','r') as f:
+#with open('../python_data/dir1/metadat.txt','r') as f:
+#       filenames=f.read().splitlines()
+with open('../01262014/ber2nh_02132014/procedural/metafile.dat','r') as f:
        filenames=f.read().splitlines()
 
 
 tn=0 # - file number
 imax=10
-jmax=10
+jmax=20
 fstep=1000 
 #filenames=filenames[0:200]
 ints=zeros((imax,jmax,shape(filenames)[0]))
-for line in filenames[0:1]:
+filenames=filenames[20:80];
+frames=range(0,size(filenames));
+data=zeros(shape=(imax,jmax,shape(frames)[0]));
+for line in filenames:
        #frames=range(100000,1001000,1000)
-       frames=range(0,size(filenames));
-       data=zeros((imax,jmax,shape(frames)[0]));
        t=multiply(frames,0.003*fstep) #timescale 0.003 with frame step (default 1000)        
        print "working on file bin: ", tn, " ", line       
        a = loadtxt(line) #timeframe number
@@ -43,7 +46,7 @@ for i in range(imax):
    for j in range(jmax):
      ints[i,j]=integrate.simps(data[i,j,:],t)/(max(t)-min(t))
                      
-n,bins,patches = P.hist(ints[1,:].reshape(-1).tolist(),normed=1,bins=20,histtype='step',color='green')
+n,bins,patches = P.hist(ints[5,:].reshape(-1).tolist(),normed=1,bins=10,histtype='step',color='green')
 #P.setp(patches, 'facecolor','g','alpha',0.75)
 x0=mean(ints[1,:].reshape(-1).tolist())
 sigma=std(ints[1,:].reshape(-1).tolist())
